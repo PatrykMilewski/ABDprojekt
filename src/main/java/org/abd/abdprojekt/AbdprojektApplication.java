d70c6853e3b445dfdf8c2871c05f9465db657036
Patryk Milewski <pmilewski@bluesoft.net.pl>
2018-10-22 20:41:50 +0200

    Initial commit, empty spring boot project

---
+++ src/main/java/org/abd/abdprojekt/AbdprojektApplication.java
@@ -0,0 +1,12 @@
+package org.abd.abdprojekt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbdprojektApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(AbdprojektApplication.class, args);
+    }
+}
